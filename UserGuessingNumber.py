67be292877148074cefd32bb2376c35011c648e3
mjavaid3 <mjavaid3356@gmail.com>
2025-04-15 00:35:37 +0500

    Update UserGuessingNumber.py

--- UserGuessingNumber.py
+++ UserGuessingNumber.py
@@ -1,15 +1,17 @@
 import random
 
-def computerguess(x):
-   anynumber= random.randint(1,x)
-   computerguess=0
-   while computerguess!= anynumber:
-       computerguess=int(input(f"Guess any number between 1 and {x}:   "))
-       if computerguess<anynumber:
-           print("Oops!The number is too low. Try again.")
-       elif computerguess>anynumber:
-           print("Oopas! The numberis too high. Try again.")
-       
-   print(f'Congratulations! You have guessed the right number{anynumber}')
+def number_hunter(upper_limit):
+    secret_number = random.randint(1, upper_limit)
+    player_attempt = 0
+    
+    while player_attempt != secret_number:
+        player_attempt = int(input(f"🔍 Find the hidden number between 1 and {upper_limit}: "))
+        
+        if player_attempt < secret_number:
+            print("⬆️ Too low! Aim higher, adventurer!")
+        elif player_attempt > secret_number:
+            print("⬇️ Too high! Come down a bit!")
+    
+    print(f'🎉 Victory! You\'ve uncovered the secret number {secret_number}!')
 
-computerguess(20)
+number_hunter(20)
